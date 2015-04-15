f39bc8b893a3172c8ee787634f1ed2929031f26f
ashima <ashimaathri@gmail.com>
2015-04-15 17:52:15 -0400

    Second workflow, more efficient, less network traffic

--- arcgis2arel.php
+++ arcgis2arel.php
@@ -3,7 +3,7 @@
   $service_url = "http://arc01.gis.vt.edu/arcgis/rest/services/Utilities/Geometry/GeometryServer";
   $label_points_url = "/labelPoints";
   $projection_url = "/project";
-  $arcgis_query = "/query?where=1=1&f=json";
+  $arcgis_query = "/query?where=1=1&f=json&inSR=102747&outSR=4326";
 
   $req = new HttpRequest($arcgis_url . $arcgis_query, HttpRequest::METH_GET);
   $req->send();
@@ -12,7 +12,6 @@
     $json = json_decode($req->getResponseBody());
     $spatialReference = $json->spatialReference->wkid;
 
-    $x = 0;
     foreach ($json->features as $building) {
       $name = $building->attributes->NAME;
       $geometry = json_encode(array($building->geometry));
@@ -21,16 +20,8 @@
         $req = new HttpRequest($service_url . $label_points_url, HttpRequest::METH_POST);
         $req->addPostFields(array('polygons' => $geometry, 'sr' => $spatialReference, 'f' => 'json'));
         $req->send();
-
         $centroid = json_decode($req->getResponseBody());
-        $geometries = array('geometryType' => 'esriGeometryPoint', 'geometries' => $centroid->labelPoints);
-
-        $req = new HttpRequest($service_url . $projection_url, HttpRequest::METH_GET);
-        $req->addQueryData(array('geometries' => json_encode($geometries), 'inSR' => $spatialReference, 'outSR' => 4326, 'f' => 'json'));
-        $req->send();
-
-        $latlong = json_decode($req->getResponseBody());
-        echo "<p>{$name} : {$latlong->geometries[0]->x}, {$latlong->geometries[0]->y}<p>";
+        echo "<p>{$name} : {$centroid->labelPoints[0]->x}, {$centroid->labelPoints[0]->y}<p>";
       }
     }
   }
