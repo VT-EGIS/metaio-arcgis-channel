7de87d9473e04b45ef96510fe7051b305d0239e8
ashima <ashimaathri@gmail.com>
2015-04-15 17:39:26 -0400

    First workflow

--- arcgis2arel.php
+++ arcgis2arel.php
@@ -1,15 +1,37 @@
 <?php
-$arcgis_url = 'http://arcgis-central.gis.vt.edu/arcgis/rest/services/vtcampusmap/Buildings/MapServer/0';
-$arcgis_query = '/query?where=1=1&f=json';
-$complete_url = $arcgis_url . $arcgis_query;
-$req = new HttpRequest($complete_url, HttpRequest::METH_GET);
-try {
+  $arcgis_url = 'http://arcgis-central.gis.vt.edu/arcgis/rest/services/vtcampusmap/Buildings/MapServer/0';
+  $service_url = "http://arc01.gis.vt.edu/arcgis/rest/services/Utilities/Geometry/GeometryServer";
+  $label_points_url = "/labelPoints";
+  $projection_url = "/project";
+  $arcgis_query = "/query?where=1=1&f=json";
+
+  $req = new HttpRequest($arcgis_url . $arcgis_query, HttpRequest::METH_GET);
   $req->send();
+
   if($req->getResponseCode() == 200) {
     $json = json_decode($req->getResponseBody());
-    var_dump($json->features);
+    $spatialReference = $json->spatialReference->wkid;
+
+    $x = 0;
+    foreach ($json->features as $building) {
+      $name = $building->attributes->NAME;
+      $geometry = json_encode(array($building->geometry));
+
+      if (!empty(trim($name))) {
+        $req = new HttpRequest($service_url . $label_points_url, HttpRequest::METH_POST);
+        $req->addPostFields(array('polygons' => $geometry, 'sr' => $spatialReference, 'f' => 'json'));
+        $req->send();
+
+        $centroid = json_decode($req->getResponseBody());
+        $geometries = array('geometryType' => 'esriGeometryPoint', 'geometries' => $centroid->labelPoints);
+
+        $req = new HttpRequest($service_url . $projection_url, HttpRequest::METH_GET);
+        $req->addQueryData(array('geometries' => json_encode($geometries), 'inSR' => $spatialReference, 'outSR' => 4326, 'f' => 'json'));
+        $req->send();
+
+        $latlong = json_decode($req->getResponseBody());
+        echo "<p>{$name} : {$latlong->geometries[0]->x}, {$latlong->geometries[0]->y}<p>";
+      }
+    }
   }
-} catch (HttpException $ex) {
-  echo $ex;
-}
 ?>
